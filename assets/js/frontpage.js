cc6b2873a24e8135bf711ac4e882e425e6a55576
Jake Chen <jakeplus@gmail.com>
2012-07-18 14:35:15 +0800

    Added etaoux members listing

--- assets/js/frontpage.js
+++ assets/js/frontpage.js
@@ -1,17 +1,24 @@
 KISSY.use('template', function(S, Template) {
 
-    var tmpl = S.one('#j-repo-template').html()
 
     S.IO.jsonp('https://api.github.com/orgs/etaoux/repos', function(data) {
-        var repos = data.data,
+        var repo_tmpl = S.one('#j-repo-template').html(),
+            repos = data.data,
             markup
 
         delete_if(repos, function(repo) {
             return repo.name === 'etaoux.github.com'
         })
-        markup = Template(tmpl).render({repos: repos})
+        markup = Template(repo_tmpl).render({repos: repos})
 
-        S.one('#repos').html(markup)
+        S.one('#repos').append(markup)
+    })
+
+    S.IO.jsonp('https://api.github.com/orgs/etaoux/members', function(data) {
+        var user_tmpl = S.one("#j-user-template").html(),
+            markup = Template(user_tmpl).render({members: data.data})
+
+        S.one('#members').html(markup)
     })
 
     function delete_if(arr, fn) {
@@ -28,4 +35,4 @@ KISSY.use('template', function(S, Template) {
             return arr.splice(i, 1)
         }
     }
-})
+});
